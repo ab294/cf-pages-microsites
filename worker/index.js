6d2e19ad61e2ec20eee7a8b77968617246544e6e
Adam Bennett <adam.bennett@specsavers.com>
2022-05-31 09:49:29 +0100

    initial commit

---
+++ worker/index.js
@@ -0,0 +1,15 @@
+addEventListener('fetch', function (event) {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(req) {
+    const path = new URL(req.url).pathname; 
+    console.log('path ', path);
+    if (path.startsWith('stores')) {
+      return fetch(`https://stores-79n.pages.dev/${path}`);
+    } else if (path.startsWith('appointments')) {
+      return fetch(`https://appointments.pages.dev/${path}`);
+    } else {
+      return fetch(`https://www.example.com/${path}`);
+    }
+}
