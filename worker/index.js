c06935da0190c7de066420fb678eec41d5337aa0
Adam Bennett <adam.bennett@specsavers.com>
2022-05-31 10:45:27 +0100

    Basic working version

--- worker/index.js
+++ worker/index.js
@@ -5,11 +5,11 @@ addEventListener('fetch', function (event) {
 async function handleRequest(req) {
     const path = new URL(req.url).pathname; 
     console.log('path ', path);
-    if (path.startsWith('stores')) {
-      return fetch(`https://stores-79n.pages.dev/${path}`);
-    } else if (path.startsWith('appointments')) {
-      return fetch(`https://appointments.pages.dev/${path}`);
+    if (path.startsWith('/stores')) {
+      return fetch(`https://stores-79n.pages.dev/${path}`, req);
+    } else if (path.startsWith('/appointments')) {
+      return fetch(`https://appointments.pages.dev/${path}`, req);
     } else {
-      return fetch(`https://www.example.com/${path}`);
+      return fetch(`https://www.example.com/${path}`, req);
     }
 }
