684d3d728d7acc0b0a6c548303bdeeccc04dbe25
Adam Bennett <adam.bennett@specsavers.com>
2022-06-01 14:02:14 +0100

    Extract microsite urls as env vars

--- worker/index.js
+++ worker/index.js
@@ -6,18 +6,28 @@ async function handleRequest(req) {
   // add auth header
   const fetchOptions = addAuthenticationHeader(req);
   
+  // Get the path from the request
   const path = new URL(req.url).pathname;
   console.log("path ", path);
 
+  // Do some basic routing based on the path
+  // NOTE: this is currently very simplistic and would need extracting into a proper service.
   if (path.startsWith("/stores")) {
-    return fetch(`https://stores-79n.pages.dev/${path}`, fetchOptions);
+    return fetch(`${STORES_APP_URL}${path}`, fetchOptions);
   } else if (path.startsWith("/appointments")) {
-    return fetch(`https://appointments.pages.dev/${path}`, fetchOptions);
+    return fetch(`${APPOINTMENT_APP_URL}${path}`, fetchOptions);
   } else {
-    return fetch(`https://default-b53.pages.dev/${path}`, fetchOptions);
+    return fetch(`${DEFAULT_APP_URL}${path}`, fetchOptions);
   }
 }
 
+/**
+ * Takes the incoming request as a parameter
+ * creates a new instance of the request with the additional authorisation header
+ * 
+ * @param {Request} req 
+ * @returns req
+ */
 const addAuthenticationHeader = (req) => {
   let headers = new Headers(req.headers);
   headers.set(PLUGIN_HEADER_NAME, PLUGIN_HEADER_VALUE)
